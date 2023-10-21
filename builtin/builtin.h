ff93c95c19ba273bb93c9832c9fd617f57beb27d
Gin-Jo <geoffrey.d2002@gmail.com>
2023-10-21 17:48:24 +0200

    echo_builtin functionnal

--- builtin/builtin.h
+++ builtin/builtin.h
@@ -6,11 +6,15 @@
 /*   By: gdornic <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 14:57:14 by gdornic           #+#    #+#             */
-/*   Updated: 2023/10/21 14:57:42 by gdornic          ###   ########.fr       */
+/*   Updated: 2023/10/21 17:46:15 by gdornic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef
+#ifndef BUILTIN_H
 # define BUILTIN_H
+# include <stdio.h>
+
+//echo builtin
+int	echo_builtin(char **strings);
 
 #endif
