acf4f16efeeb38f7eee26d30fa02cd456a189e95
Gin-Jo <geoffrey.d2002@gmail.com>
2023-10-21 18:45:08 +0200

    header ready

--- builtin/builtin.h
+++ builtin/builtin.h
@@ -6,7 +6,7 @@
 /*   By: lseiberr <lseiberr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 14:57:14 by gdornic           #+#    #+#             */
-/*   Updated: 2023/10/21 18:33:20 by lseiberr         ###   ########.fr       */
+/*   Updated: 2023/10/21 18:44:07 by gdornic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
 # include <sys/stat.h>
 # include <fcntl.h>
 
-//cd_builtin.c
+//cd builtin
 int	cd_builtin(char *path);
 
 //echo builtin
@@ -30,5 +30,17 @@ int	echo_builtin(char **strings);
 
 //pwd builtin
 int	pwd_builtin(void);
+
+//export builtin
+int	export_builtin(char **var);
+
+//unset builtin
+int	unset_builtin(char **var);
+
+//env builtin
+int	env_builtin(char *envp[]);
+
+//exit
+int	exit_builtin(int n);
 
 #endif
