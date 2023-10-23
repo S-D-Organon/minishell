dcad2a7136592b23e64696c17a301eab094080d4
lcssbrs <lucas.seib@icloud.com>
2023-10-23 16:18:26 +0200

    tes

--- builtin/env_builtin.c
+++ builtin/env_builtin.c
@@ -6,17 +6,55 @@
 /*   By: lseiberr <lseiberr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 15:06:04 by lseiberr          #+#    #+#             */
-/*   Updated: 2023/10/23 15:42:50 by lseiberr         ###   ########.fr       */
+/*   Updated: 2023/10/23 16:10:49 by lseiberr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "builtin.h"
+//#include "builtin.h"
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 extern char	**environ;
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*new_str;
+	int		i;
+	int		j;
+
+	if (!s1 || !s2)
+		return (0);
+	new_str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (new_str == NULL)
+		return (0);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		new_str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		new_str[i] = s2[j];
+		i++;
+		j++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
+}
 
 void	env_builtin(void)
 {
@@ -29,15 +67,15 @@ void	env_builtin(void)
 		i++;
 	}
 }
-/*
+
 int	main(int ac, char **ag, char **env)
 {
 	char	*tut;
 
-	environ = env;
-	tut = getenv("TMPDIR");
-	memcpy(tut, "test", 5);
-	//printf("%s\n", getenv("TMPDIR"));
-	env_builtin();
+	//environ = env;
+	tut = getenv("PWD");
+	//memcpy(tut, "test", 5);
+	//printf("%s\n", tut);
+	execve(ft_strjoin("/bin/", ag[1]), ag + 1, env);
+	//env_builtin();
 }
-*/
