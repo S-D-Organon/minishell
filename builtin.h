17dc8f03c8abaabe4a08f130973c277d95bfe28d
lcssbrs <lucas.seib@icloud.com>
2023-10-24 10:23:27 +0200

    add builtin.h

---
+++ builtin.h
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lseiberr <lseiberr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 10:18:04 by lseiberr          #+#    #+#             */
+/*   Updated: 2023/10/24 10:22:31 by lseiberr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUILTIN_H
+# define BUILTIN_H
+
+# include <unistd.h>
+# include <string.h>
+# include <limits.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <dirent.h>
+# include <errno.h>
+# include <termios.h>
+# include <curses.h>
+
+//cd builtin
+int		cd_builtin(char *path);
+
+//echo builtin
+int		echo_builtin(char **strings);
+
+//env builtin
+void	env_builtin(void);
+
+//pwd builtin
+int	pwd_builtin(void);
+
+#endif
